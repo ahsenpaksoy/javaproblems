de80cc48482c818b91e8c04798ca6df31ddac523
ahsenpaksoy <ahsenpaksoy@gmail.com>
2024-11-13 23:21:16 +0100

    repeating numbers

---
+++ GeeksProblems/src/com/arrays/ArrayOfDuplicatingNumbersInArray.java
@@ -0,0 +1,60 @@
+package com.arrays;
+
+import java.util.*;
+
+import static java.util.Arrays.sort;
+
+
+public class ArrayOfDuplicatingNumbersInArray {
+
+    public static List<Integer> getThearry(int[] mylist){
+
+        List<Integer> foundArray = new ArrayList<>();
+
+        for (int i = 0; i < mylist.length; i++) {
+            for (int j = i+1; j <mylist.length ; j++) {
+                if(mylist[i]== mylist[j]){
+                    boolean isWithin = false;
+                    for (int k = 0; k < foundArray.size(); k++) {
+                        if(mylist[i] == foundArray.get(k)){
+                            isWithin = true;
+                        }
+
+                    }
+                    if (!isWithin){
+                        foundArray.add(mylist[i]);
+                    }
+                }
+
+            }
+
+        }
+
+        Collections.sort(foundArray);
+
+        return foundArray;
+    }
+
+    public static List<Integer> getDuplicatingNumbers(List<Integer> mylist) {
+        Set<Integer> seen = new HashSet<>();
+        Set<Integer> duplicates = new HashSet<>();
+        List<Integer> foundArray = new ArrayList<>();
+
+        for (Integer num : mylist) {
+            // Check if the number has already been seen
+            if (seen.contains(num)) {
+                // If it's the first time we encounter this duplicate, add it to the foundArray
+                if (!duplicates.contains(num)) {
+                    foundArray.add(num);
+                    duplicates.add(num);
+                }
+            } else {
+                // Add the number to the seen set
+                seen.add(num);
+            }
+        }
+
+        return foundArray;
+    }
+
+}
--- GeeksProblems/src/com/arrays/Runner.java
+++ GeeksProblems/src/com/arrays/Runner.java
@@ -1,5 +1,10 @@
 package com.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Runner {
 
     public static void main(String[] args) {
@@ -9,5 +14,15 @@ public class Runner {
         System.out.println(miss);
         System.out.println(MissingNumberInArray.getMissingNumberWithXor(arr));
         System.out.println(MissingNumberInArray.getMissingNumberSet(arr));
+
+
+        int[] mylist =  {1,1,2,2};
+        List<Integer> resultList = new ArrayList<>();
+        resultList = ArrayOfDuplicatingNumbersInArray.getThearry(mylist);
+        System.out.println(resultList);
+
+        System.out.println(ArrayOfDuplicatingNumbersInArray.getDuplicatingNumbers(Arrays.stream(mylist)
+                .boxed() // Convert each int to Integer
+                .collect(Collectors.toList())));
     }
 }
