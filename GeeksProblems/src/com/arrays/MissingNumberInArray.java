7d37cf8a6d157040a76df711def44fca42fd1f39
ahsenpaksoy <ahsenpaksoy@gmail.com>
2024-11-14 21:03:22 +0100

    update

--- GeeksProblems/src/com/arrays/MissingNumberInArray.java
+++ GeeksProblems/src/com/arrays/MissingNumberInArray.java
@@ -1,9 +1,16 @@
 package com.arrays;
 
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
+/*
+You are given an array arr of size n - 1 that contains distinct integers in the range from 1 to n (inclusive).
+This array represents a permutation of the integers from 1 to n with one element missing. Your task is to
+identify and return the missing element.
 
+Input: arr[] = [1, 2, 3, 5]
+Output: 4
+Explanation: All the numbers from 1 to 5 are present except 4.
+
+ */
 public class MissingNumberInArray {
 
     public static int getMissingNumber(int arr[]) {
@@ -18,8 +25,6 @@ public class MissingNumberInArray {
                     found = true;
                     break;
                 }
-
-
             }
             if(!found){
                 missingNumber = i;
