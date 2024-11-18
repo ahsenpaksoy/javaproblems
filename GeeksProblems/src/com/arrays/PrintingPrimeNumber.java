a0d3b51830d49464beeab00baba3ba7c3043a4e2
ahsenpaksoy <ahsenpaksoy@gmail.com>
2024-11-18 19:46:55 +0100

    update on PrintingPrimeNumber

--- GeeksProblems/src/com/arrays/PrintingPrimeNumber.java
+++ GeeksProblems/src/com/arrays/PrintingPrimeNumber.java
@@ -1,8 +1,17 @@
 package com.arrays;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrintingPrimeNumber {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Pozitif bir sayi giriniz :");
+        int n = scan.nextInt();
+        PrintingPrimeNumber.printPrime(n);
+    }
 
     private static boolean isPrime(int n){
         // n sayısının asal olup olmadıgını kontrol eder. asal ise True degilse False döner
