d875c299762247ee12febfbe66a890edb30bdcd0
ahsenpaksoy <ahsenpaksoy@gmail.com>
2024-11-18 19:47:20 +0100

    update on SecondLargestNumber

--- GeeksProblems/src/com/arrays/SecondLargestNumber.java
+++ GeeksProblems/src/com/arrays/SecondLargestNumber.java
@@ -9,6 +9,18 @@ Note: The second largest element should not be equal to the largest element.
  */
 public class SecondLargestNumber {
 
+    public static void main(String[] args) {
+        int[] arr1 = {3, 5, 2, 5, 7, 7, 1};
+        Integer result = SecondLargestNumber.getNumber(arr1);
+        if (result != null){
+            System.out.println("The second largest number is: " + result);
+        } else {
+            System.out.println("There is no second largest number.");
+        }
+        System.out.println();
+        System.out.println(SecondLargestNumber.getSecondLargestWithSort(arr1));
+    }
+
     public static int getNumber(int[] arr){
         if(arr == null || arr.length<2){
             return -1;
